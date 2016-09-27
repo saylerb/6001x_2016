44f1b2dd35039ecc01758b0d29c803f22055ad87
Brian Sayler <sayler.b@gmail.com>
2016-09-27 12:27:59 -0600

    add method for calculating balance after 12 minimum payments

--- ps2/minimum-payment/minimum_payment_test.py
+++ ps2/minimum-payment/minimum_payment_test.py
@@ -1,31 +1,31 @@
 import unittest
 
-from minimum_payment import calculate_minimum_payment
+from minimum_payment import balance_after_12_minimum_payments
 
 class TestMinimumPayment(unittest.TestCase):
 
     def test_case_one(self):
         self.assertEqual(
             {'Remaining balance': 31.38},
-            calculate_minimum_payment(42, 0.2, 0.04)
+            balance_after_12_minimum_payments(42, 0.2, 0.04)
         )
 
     def test_case_two(self):
        self.assertEqual(
            {'Remaining balance': 361.61},
-           calculate_minimum_payment(484, 0.2, 0.04)
+           balance_after_12_minimum_payments(484, 0.2, 0.04)
        )
 
     def test_case_three(self):
         self.assertEqual(
            {'Remaining balance': 3147.67},
-           calculate_minimum_payment(4213, 0.2, 0.04)
+           balance_after_12_minimum_payments(4213, 0.2, 0.04)
         )
 
     def test_case_four(self):
         self.assertEqual(
            {'Remaining balance': 3617.62},
-           calculate_minimum_payment(4842, 0.2, 0.04)
+           balance_after_12_minimum_payments(4842, 0.2, 0.04)
         )
 
 if __name__ == '__main__':
